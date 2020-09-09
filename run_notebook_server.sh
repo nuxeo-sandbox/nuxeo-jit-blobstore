8915a3106bf2bbc5560afdc2a8bb21d84b84d0fd
Thierry Delprat <tdelprat@nuxeo.com>
2020-09-09 18:38:14 +0000

    XP-28764 update notebook config

--- run_notebook_server.sh
+++ run_notebook_server.sh
@@ -11,4 +11,4 @@ echo $M2_HOME
 
 docker rm benchnb
 
-docker run -e NXUSER -e NXPWD -e MONGOUSER -e MONGOPWD -e PARENTDIR --name benchnb --rm -p 127.0.0.1:8888:8888 -v $PARENTDIR/nuxeo-datagen-cli/scripts:/nxbench/scripts -v $PARENTDIR/notebooks:/nxbench/notebooks -v $PARENTDIR/blobstore:/nxbench/blobs -v /home/tdelprat/bench/injector/data:/nxbench/data --net host nxbench/notebook:latest
+docker run --env-file $PARENTDIR/notebook.env -e PARENTDIR --name benchnb --rm -p 127.0.0.1:8888:8888 -v $PARENTDIR/nuxeo-datagen-cli/scripts:/nxbench/scripts -v $PARENTDIR/notebooks:/nxbench/notebooks -v $PARENTDIR/blobstore:/nxbench/blobs -v /home/tdelprat/bench/injector/data:/nxbench/data -v /apps/nuxeo/conf/:/nxbench/nuxeo-conf:ro --net host nxbench/notebook:latest
